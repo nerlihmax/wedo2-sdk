fbc47e22c4b452dc05804a5c108b68e2fe1d344f
Nerlih Max <nerlihmax@yandex.ru>
2022-05-19 19:17:39 +0700

    release: v0.1.0-rc

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,27 @@
-import log from 'loglevel';
-import { tap } from 'ramda';
-import { match } from 'ts-pattern';
-
 import {
   connect,
   setAttachIoListener,
   setSensorValueListener,
-} from './connection';
+} from '../src/connection';
 import { setLedColor } from './wedo2/cmds/led';
 import { setMotorState } from './wedo2/cmds/motor';
+import { Wedo2PhysicalPort } from './wedo2/devices';
 import { wedo2PhysicalPort } from './wedo2/devices';
+import { Wedo2LedColor } from './wedo2/devices/led';
 import { wedo2LedColor } from './wedo2/devices/led';
+import { Wedo2TiltSensorDirection } from './wedo2/devices/tilt';
 import { wedo2TiltSensorDirection } from './wedo2/devices/tilt';
 
-const wait =
-  (timeout: number) =>
-  <T>(t: T) =>
-    new Promise<T>((resolve) => setTimeout(() => resolve(t), timeout));
-
-log.setDefaultLevel(
-  (process.env.NODE_ENV ?? 'production') === 'production' ? 'silent' : 'debug'
-);
-
-connect()
-  .then(
-    tap((conn) =>
-      console.log(`\nuserspace: подключился к устройству ${conn.deviceName}\n`)
-    )
-  )
-  .then(
-    setAttachIoListener({
-      attach: (device) =>
-        console.log(
-          `\nuserspace: подключено ${device.tag} устройство в порт ${device.port}\n`
-        ),
-      detach: (event) =>
-        console.log(
-          `\nuserspace: из порта ${event.port} отключено устройство\n`
-        ),
-    })
-  )
-  .then(
-    setSensorValueListener((value) =>
-      console.log(
-        `\nuserspace: ${`${match(value)
-          .with(
-            { device: { tag: 'tilt' } },
-            (value) =>
-              `датчик наклона наклонен ${match(value.direction)
-                .with(wedo2TiltSensorDirection.BACKWARD, () => 'назад')
-                .with(wedo2TiltSensorDirection.FORWARD, () => 'вперед')
-                .with(wedo2TiltSensorDirection.LEFT, () => 'влево')
-                .with(wedo2TiltSensorDirection.RIGHT, () => 'направо')
-                .with(wedo2TiltSensorDirection.NETURAL, () => 'нейтрально')
-                .exhaustive()}`
-          )
-          .with(
-            { device: { tag: 'distance' } },
-            (value) => `датчик растояния отдален на ${value.distance}`
-          )
-          .exhaustive()}`}\n`
-      )
-    )
-  )
-  .then(wait(10000))
-  .then(
-    tap((conn) => {
-      console.log('включаю мотор');
-      console.log('порты: ', conn.ports);
-
-      const device = conn.ports[wedo2PhysicalPort.PORT1];
-      if (device.tag === 'motor') {
-        setMotorState(device, -80)(conn);
-      }
-    })
-  );
+export {
+  connect,
+  setAttachIoListener,
+  setSensorValueListener,
+  setLedColor,
+  setMotorState,
+  Wedo2PhysicalPort,
+  wedo2PhysicalPort,
+  Wedo2LedColor,
+  wedo2LedColor,
+  Wedo2TiltSensorDirection,
+  wedo2TiltSensorDirection,
+};
